0ef57f9c4682d86255f01a3af88709c3b5bb72a2
Agustin Aguilera <agustin.aguilera424@gmail.com>
2020-09-04 17:27:09 -0300

    Inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Inicio = () => {
     return (
-        <div>
-            Inicio
+
+        <div class="jumbotron mt-4">
+            <h1 class="display-4">Bienvenido</h1>
+            <p class="lead">Cajero Automático TRZ 2 : Versión alpha 0.01</p>
+            <hr class="my-4" />
+            <p>Muchas gracias por visitar este proyecto</p>
+
         </div>
     )
 }
