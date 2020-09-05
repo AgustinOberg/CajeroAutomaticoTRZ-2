71b0aeb106512105a9a9a782966c2cab1b05b6fb
Agustin Aguilera <agustin.aguilera424@gmail.com>
2020-09-05 10:15:34 -0300

    Diseño del HomeBank

--- src/components/Homebank.jsx
+++ src/components/Homebank.jsx
@@ -2,8 +2,71 @@ import React from 'react'
 
 const Homebank = () => {
     return (
-        <div>
-            Homebank
+        <div className="mt-3">
+            <h4 className="text-center">Home Banking</h4>
+            <hr />
+            <div className="row justify-content-center">
+
+
+                <div className="col-4">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            Caja de ahorro en ARS
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item text-center" key="1">Saldo: <strong>$500</strong></li>
+                            <button type="button" className="list-group-item list-group-item-action">Transferencia a terceros <span className="float-right">- $500</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                        </ul>
+                    </div>
+                    <button className="btn btn-warning btn-sm mt-2 btn-block">Ver más...</button>
+                </div>
+
+
+                <div className="col-4">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            Caja de ahorro en USD
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item text-center" key="1">Saldo: <strong>$500</strong></li>
+                            <button type="button" className="list-group-item list-group-item-action">Transferencia a terceros <span className="float-right">- $500</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                        </ul>
+                    </div>
+                    <button className="btn btn-warning btn-sm mt-2 btn-block">Ver más...</button>
+                </div>
+
+
+
+
+                <div className="col-4">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            Cuenta corriente
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item text-center" key="1">Saldo: <strong>$500</strong></li>
+                            <button type="button" className="list-group-item list-group-item-action">Transferencia a terceros <span className="float-right">- $500</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                            <button type="button" className="list-group-item list-group-item-action">Depósito de fondos <span className="float-right">$1800</span> </button>
+                        </ul>
+                    </div>
+                    <button className="btn btn-warning btn-sm mt-2 btn-block">Ver más...</button>
+                </div>
+
+
+
+            </div>
+
         </div>
     )
 }
